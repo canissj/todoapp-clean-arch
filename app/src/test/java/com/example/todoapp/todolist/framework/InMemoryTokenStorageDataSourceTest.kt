085969363b372d921d39402c7de4005bd071d7fd
canissj <santiago.canosa@mercadolibre.com>
2021-08-02 00:14:15 -0300

    tests for in memory token storage data source

---
+++ app/src/test/java/com/example/todoapp/todolist/framework/InMemoryTokenStorageDataSourceTest.kt
@@ -0,0 +1,41 @@
+package com.example.todoapp.todolist.framework
+
+import com.example.todoapp.todolist.framework.storage.InMemoryTokenStorageDataSource
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class InMemoryTokenStorageDataSourceTest {
+
+    private lateinit var inMemoryTokenStorageDataSource: InMemoryTokenStorageDataSource
+
+    @Before
+    fun setUp() {
+        inMemoryTokenStorageDataSource = InMemoryTokenStorageDataSource()
+    }
+
+    @Test
+    fun `getToken should return null when no token is available`() {
+        // given
+
+        // when
+        val token = inMemoryTokenStorageDataSource.getToken()
+
+        // then
+        assertNull(token)
+    }
+
+    @Test
+    fun `getToken should return token`() {
+        // given
+        val expectedToken = "some_token"
+        inMemoryTokenStorageDataSource.saveToken(expectedToken)
+
+        // when
+        val token = inMemoryTokenStorageDataSource.getToken()
+
+        // then
+        assertEquals(expectedToken, token)
+    }
+}
